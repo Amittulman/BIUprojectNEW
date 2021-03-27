ce8ecb1b873eb50ff27f075f8f5b78cd955421f2
dorgamliel <47919011+dorgamliel@users.noreply.github.com>
2021-03-27 16:11:32 +0300

    Frontend now receives data from backend (ALL tasks), and ables to submit a single task.

--- frontend/src/components/NewTasks.js
+++ frontend/src/components/NewTasks.js
@@ -47,21 +47,35 @@ class NewTasks extends Component {
     const nam = event.target.name;
     const val = event.target.value;
     this.setState({[nam]: val})
+    console.log(nam  + ", " +  val);
   }
 
 
   render() {
-    let title = <div onChange={this.handleChange}> Title:&nbsp;&nbsp; <input name='title' type='text'/></div>;
-    let duration = <div onChange={this.handleChange}> Duration:&nbsp;&nbsp;<input name='duration' type='text'/></div>;
-    let priority = <div onChange={this.handleChange}>Priority:&nbsp;&nbsp;<input name='priority' type='text'/></div>;
-    let category_id = <div onChange={this.handleChange}>Category:&nbsp;&nbsp;<input name='category_id' type='text'/></div>;
-    let constraints = <div onChange={this.handleChange}>Constraints:&nbsp;&nbsp;<input name='constraints' type='text'/></div>;
+    let title = <div id='task_elm' onChange={this.handleChange}> Title:&nbsp;&nbsp; <input name='title' type='text'/></div>;
+    let duration = <div id='task_elm' onChange={this.handleChange}> Duration:&nbsp;&nbsp;<input id='nums_input' name='duration' type='text'/></div>;
+    let priority = <div id='task_elm' onChange={this.handleChange}>Priority:&nbsp;&nbsp;
+      <select id='priority_options' name='priority' defaultValue={this.state.priority} onChange={this.handleChange}>
+      <option value="0">None</option>
+      <option value="1">Low</option>
+      <option value="2">Medium</option>
+      <option value="3">High</option>
+    </select></div>;
+    let category_id = <div id='task_elm' onChange={this.handleChange}>Category:&nbsp;&nbsp;<input name='category_id' type='text'/></div>;
+    let constraints = <div id='task_elm' onChange={this.handleChange}>Constraints:&nbsp;&nbsp;<input name='constraints' type='text'/></div>;
+    const task = [title, duration, priority, category_id, constraints];
+    const tasks = [];
+    let i = 1;
+    for (i=1;i<2;i++) {
+      tasks.push(<div><b>Task{i}:</b>{task}</div>);
+    }
     return (
         <div className="App">
           <header className="App-header">
             <form onSubmit={this.onSubmitHandler}>
               <h1>Enter your tasks</h1>
-              {title}{duration}{priority}{category_id}{constraints}
+              {tasks}
+              <br/>
               <input className="btn btn-primary btn-md" type='submit'/>
             </form><br/>
             <GetTasks/>
